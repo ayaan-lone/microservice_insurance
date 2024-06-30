b64435ae9e1072a8983da4795aad11d9f2c8804e
Himanshu Singh Dhapola <himanshu.dhapola@xebia.com>
2024-06-30 12:21:42 +0530

    Health check controller added

--- src/main/java/com/microservice/insurance/InsuranceApplication.java
+++ src/main/java/com/microservice/insurance/InsuranceApplication.java
@@ -1,13 +1,20 @@
 package com.microservice.insurance;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InsuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsuranceApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
