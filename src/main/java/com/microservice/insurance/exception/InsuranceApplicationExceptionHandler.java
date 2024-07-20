7f1bde5d5298026a976a37492af84eb2d060f951
Aryan Saini <aryan.saini@xebia.com>
2024-07-20 23:10:01 +0530

    Insurance final (Test cases remain)

--- src/main/java/com/microservice/insurance/exception/InsuranceApplicationExceptionHandler.java
+++ src/main/java/com/microservice/insurance/exception/InsuranceApplicationExceptionHandler.java
@@ -3,6 +3,7 @@ package com.microservice.insurance.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 public class InsuranceApplicationExceptionHandler {
@@ -11,5 +12,11 @@ public class InsuranceApplicationExceptionHandler {
 	ResponseEntity<Object> handleTransactionException(InsuranceApplicationException insuranceApplicationException) {
 		return ResponseEntity.status(insuranceApplicationException.getHttpStatus())
 				.body(insuranceApplicationException.getMessage());
+	}
+	
+	@ExceptionHandler(value = { HttpClientErrorException.class })
+	ResponseEntity<Object> handleHttpClientErrorException(HttpClientErrorException httpClientErrorException) {
+		return ResponseEntity.status(httpClientErrorException.getStatusCode())
+				.body(httpClientErrorException.getMessage());
 	}
 }
